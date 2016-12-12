d5eed6ca633467252337f6acce33b99015d62acb
Vino <vinohu@163.com>
2016-12-12 23:11:15 +0800

    Create 2.3.1.py

---
+++ Chapter2/2.3.1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+name = "ada lovelace"
+# name后面的“.”让Python对name变量执行“title”指定的操作
+# title() 让每个单词的首字母大写
+print(name.title())
+
+name = "ADa LOvelace"
+print(name)
+# lower 表示将所有的单词转化为小写字母，可以将所有输入的英文转变为固定的格式，再通过其他方式按照自己的需求转变
+print(name.lower())
+# upper 表示全部转为大写
+print(name.upper())
