5e4dbaec10d08e425a4f01097861a4932b238bae
Vino <vinohu@163.com>
2016-12-14 23:07:37 +0800

    Create 2.3.4.py

---
+++ Chapter2/2.3.4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+favorite_language = " Py thon "
+print(favorite_language)
+# rstrip 表示删除字符串末尾的空格
+print(favorite_language.rstrip())
+# lstrip 表示删除字符串前面的空格
+print(favorite_language.lstrip())
+# strip 表示删除字符串前后的空格
+print(favorite_language.strip())
