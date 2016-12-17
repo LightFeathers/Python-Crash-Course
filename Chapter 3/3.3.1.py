a6bf20911417a4a2bffc085b87f42ce418d2ff21
Vino <vinohu@163.com>
2016-12-17 23:07:26 +0800

    Create 3.3.1.py

---
+++ Chapter 3/3.3.1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+# sort() 表示对列表进行永久性排序（还有非永久的？）
+cars.sort()
+print(cars)
+
+# reverse=True 将列表以倒序排列
+cars.sort(reverse=True)
+print(cars)
