d94274c3d8903318969c4e5ac7422dd1abb6ab27
Vino <vinohu@163.com>
2016-12-18 21:41:27 +0800

    Create 3.3.2.py

---
+++ Chapter 3/3.3.2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+# sorted() 使元素能够在本次执行时以制定顺序排列，但不影响元素在原始列表中的顺序
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
