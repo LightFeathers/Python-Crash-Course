754e7d3ce37322558003645836684f88f6e0e6ec
Vino <vinohu@163.com>
2016-12-21 23:54:17 +0800

    Create 4.4.2.py

---
+++ Chapter 4/4.4.2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+players = ['charles', 'martina', 'micheal', 'florence', 'eli']
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
