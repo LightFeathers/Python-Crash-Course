ceeb0d6f8b6ebd40a44750fdd79bddb4e8070011
Vino <vinohu@163.com>
2016-12-25 23:36:54 +0800

    Create 5.2.7.py

---
+++ Chapter 5/5.2.7.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+banned_uesrs = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_uesrs:
+    print(user.title() + ", you can post a response if you wish.")
